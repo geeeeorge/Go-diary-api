fcef0fca4a6f0d4945348b87676b5af99a5ad69f
geeeeorge <yuto.george@i.softbank.jp>
2022-12-25 16:28:29 +0900

    add check to model

--- model/memo.go
+++ model/memo.go
@@ -8,6 +8,7 @@ type Memo struct {
 	Date      *time.Time `json:"date" db:"date"`
 	Title     string     `json:"title" db:"title"`
 	Content   string     `json:"content" db:"content"`
+	Check     bool       `json:"check" db:"check"`
 	CreatedAt *time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at" db:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at" db:"deleted_at"`
